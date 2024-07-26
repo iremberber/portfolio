dc4a034ae650969923d782c8c930a5eaddb73182
sevda <iremberber0@gmail.com>
2024-07-26 20:13:06 +0300

    update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,17 @@ import './topbar.scss'
 export default function Topbar() {
   return (
     <div className='topbar'>
-      
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className='logo'>genius</a>
+          <div className="itemcontainer">
+            
+          </div>
+        </div>
+        <h1>Hello</h1>
+        <div className="right"> This is right</div>
+
+      </div>
     </div>
   )
 }
