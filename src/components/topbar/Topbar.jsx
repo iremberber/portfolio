7f902907fab835e0d7c6a1590b967ff346adfa12
sevda <iremberber0@gmail.com>
2024-07-27 16:07:27 +0300

    idk

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,17 @@
-import './topbar.scss'
+import './topbar.scss';
+import {Person , Mail} from '@material-ui/icons';
 
 export default function Topbar() {
   return (
     <div className='topbar'>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className='logo'>genius</a>
+          <a href="#intro" className='logo'>Anasayfa</a>
           <div className="itemcontainer">
-            
+            <Person/>
+            <span>551 699 38 61</span>
+            <Mail/>
+            <span>iremberber0@gmail.com</span>
           </div>
         </div>
         <h1>Hello</h1>
